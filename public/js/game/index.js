ab979192889882c32b322853e41b86a1720b4a05
Ryder Spees <ryderspees@gmail.com>
2024-02-07 12:40:21 -0500

    Add basic directory structure

---
+++ public/js/game/index.js
@@ -0,0 +1,31 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    // Load assets
+}
+
+function create() {
+    // Create game entities
+}
+
+function update() {
+    // Game loop
+}
