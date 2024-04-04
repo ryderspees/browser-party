2f822c3d80d8f716e49d79d96458c02d31add7b0
Ryder Spees <ryderspees@gmail.com>
2024-04-04 16:34:00 -0400

    Add name box

--- public/game/scenes/MainMenuScene.js
+++ public/game/scenes/MainMenuScene.js
@@ -4,14 +4,26 @@ class MainMenuScene extends Phaser.Scene {
     }
 
     create() {
-        // Add menu items and logic to start the game, etc.
         this.add.text(100, 100, 'Main Menu', { fill: '#000000' });
-        this.add.text(100, 150, 'Press SPACE to start', { fill: '#000000' });
-
-        this.input.keyboard.on('keydown-SPACE', () => {
-            this.scene.start('GameScene');
-        });
+        this.add.text(100, 150, 'Enter Name:', { fill: '#000000' });
+        showTextInputPopup();
+        this.scene.start('GameScene');
     }
+}
+
+function showTextInputPopup() {
+    var textInput = document.getElementById('nameInput');
+    textInput.style.display = 'block';
+    textInput.focus();
+
+    textInput.addEventListener('keypress', function onEnter(event) {
+        if (event.key === 'Enter') {
+            textInput.style.display = 'none';
+            textInput.removeEventListener('keypress', onEnter);
+            // Process the input value here
+            console.log(textInput.value);
+        }
+    });
 }
 
 export default MainMenuScene;
